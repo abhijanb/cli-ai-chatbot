d6b91d631394d48f16d5c3dd3db47b30e5d41d5f
abhijanb <abhijan097@gmail.com>
2026-05-11 22:59:28 +0545

    Refactor content handling to maintain conversation context and ensure JSON response format

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 client = genai.Client(api_key=os.getenv("API_KEY"))
+contents = [
+    {"role": "user", "parts": [{"text": "Return response in JSON format with keys: answer"}]}
+]
 # model = client.models.list()
 # print(model)
 # for i in model:
 #     print(f"${i} \n")
 while True:
-    added_part = "Return response in JSON format with keys: answer"
     question=input("what you want to know ")
+    contents.append({"role": "user", "parts": [{"text": question}]})
     if question.lower() == "exit":
         break
-    response = client.models.generate_content(model="gemini-2.5-flash",contents=question + added_part)
+    response = client.models.generate_content(model="gemini-2.5-flash",contents=contents)
+    contents.append({"role": "model", "parts": [{"text": response.text}]})
     print(response.text)
     
